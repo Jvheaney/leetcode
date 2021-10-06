5cf0f42854c3a699b3a4e8ebe385a5da24ca4d65
James Heaney <40678238+Jvheaney@users.noreply.github.com>
2021-10-06 03:52:07 -0400

    Update Solution.js

--- 1270_shuffle_the_array/Solution.js
+++ 1270_shuffle_the_array/Solution.js
@@ -4,7 +4,7 @@
  * @return {number[]}
  */
 var shuffle = function(nums, n) {
-    var ret = [n];
+    var ret = [n*2];
     for(var i = 0; i < n; i++){
         ret[i*2] = nums[i];
         ret[(i*2) + 1] = nums[i + n];
